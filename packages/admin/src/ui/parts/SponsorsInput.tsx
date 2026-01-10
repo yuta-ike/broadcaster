10038fded636b1ece3b5a954c1a10e3c9fce485d
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2026-01-10 23:28:51 +0900

    fix: SponsorsInput

--- packages/admin/src/ui/parts/SponsorsInput.tsx
+++ packages/admin/src/ui/parts/SponsorsInput.tsx
@@ -1,4 +1,6 @@
 import { CheckboxOption } from "broadcaster-components/control/checkbox.js"
+import { RadioOption } from "broadcaster-components/control/radio.js"
+import { Activity, useId, useState } from "react"
 import type { Label, Sponsor } from "../../domain/model/Sponsor.js"
 import { LabelDisplay } from "../components/label-display.js"
 
@@ -17,71 +19,100 @@ export const SponsorsInput = ({
 }: Props) => {
   const grouped = groupByLabels(sponsors)
 
-  return (
-    <div className="overflow-hidden rounded border border-slate-200">
-      <div className="flex flex-wrap gap-4 p-4">
-        <CheckboxOption
-          checked={values.length === sponsors.length}
-          onChange={(e) => {
-            if (e.target.checked) {
-              onChange(sponsors.map(({ id }) => id))
-            } else {
-              onChange([])
-            }
-          }}
-        >
-          全選択
-        </CheckboxOption>
-      </div>
-      <hr className="border-t border-t-slate-200" />
-      <div className="flex flex-wrap gap-4 p-4">
-        {labels.map((label) => {
-          const labeled = new Set(grouped.get(label.id) ?? [])
-          if (labeled.size === 0) {
-            return null
-          }
+  const [checkedOption, setCheckedOption] = useState<"Label" | "Sponsor">(
+    "Label",
+  )
 
-          const selected = new Set(values).intersection(labeled)
-          return (
-            <CheckboxOption
-              key={label.id}
-              checked={0 < selected.size}
-              indeterminate={0 < selected.size && selected.size < labeled.size}
-              onChange={(e) => {
-                if (e.target.checked) {
-                  onChange([...values, ...labeled])
-                } else {
-                  onChange(values.filter((id) => !labeled.has(id)))
-                }
-              }}
-            >
-              <LabelDisplay label={label} />
-            </CheckboxOption>
-          )
-        })}
-      </div>
-      <details className="border-slate-200 border-t">
-        <summary className="cursor-pointer select-none px-4 py-2 hover:bg-slate-50">
-          個別に指定する
-        </summary>
-        <div className="flex flex-wrap gap-4 p-4">
-          {sponsors.map((sponsor) => (
-            <CheckboxOption
-              key={sponsor.id}
-              checked={values.includes(sponsor.id)}
-              onChange={(e) => {
-                if (e.target.checked) {
-                  onChange([...values, sponsor.id])
-                } else {
-                  onChange(values.filter((id) => id !== sponsor.id))
-                }
-              }}
-            >
-              {sponsor.name}
-            </CheckboxOption>
-          ))}
+  const id = useId()
+
+  return (
+    <div>
+      <div className="flex flex-col rounded-lg border border-slate-200">
+        <div className="group p-3">
+          <RadioOption
+            name={`${id}-sponsor-input-tab`}
+            checked={checkedOption === "Label"}
+            onChange={(e) => {
+              if (e.target.checked) {
+                setCheckedOption("Label")
+                onChange([])
+              }
+            }}
+          >
+            ラベルで指定する
+          </RadioOption>
+          <Activity mode={checkedOption === "Label" ? "visible" : "hidden"}>
+            <div className="flex flex-col pl-7">
+              <hr className="broder-t my-3 border-t-slate-200" />
+              <div className="flex flex-wrap gap-4">
+                {labels.map((label) => {
+                  const labeled = new Set(grouped.get(label.id) ?? [])
+                  if (labeled.size === 0) {
+                    return null
+                  }
+
+                  const selected = new Set(values).intersection(labeled)
+                  return (
+                    <CheckboxOption
+                      key={label.id}
+                      checked={0 < selected.size}
+                      indeterminate={
+                        0 < selected.size && selected.size < labeled.size
+                      }
+                      onChange={(e) => {
+                        if (e.target.checked) {
+                          onChange([...values, ...labeled])
+                        } else {
+                          onChange(values.filter((id) => !labeled.has(id)))
+                        }
+                      }}
+                    >
+                      <LabelDisplay label={label} />
+                    </CheckboxOption>
+                  )
+                })}
+              </div>
+            </div>
+          </Activity>
         </div>
-      </details>
+        <hr className="border-t border-t-slate-200" />
+        <div className="group p-3">
+          <RadioOption
+            name={`${id}-sponsor-input-tab`}
+            checked={checkedOption === "Sponsor"}
+            onChange={(e) => {
+              if (e.target.checked) {
+                setCheckedOption("Sponsor")
+                onChange([])
+              }
+            }}
+          >
+            個別に指定する
+          </RadioOption>
+          <Activity mode={checkedOption === "Sponsor" ? "visible" : "hidden"}>
+            <div className="flex flex-col pl-7">
+              <hr className="broder-t my-3 border-t-slate-200" />
+              <div className="flex flex-wrap gap-4">
+                {sponsors.map((sponsor) => (
+                  <CheckboxOption
+                    key={sponsor.id}
+                    checked={values.includes(sponsor.id)}
+                    onChange={(e) => {
+                      if (e.target.checked) {
+                        onChange([...values, sponsor.id])
+                      } else {
+                        onChange(values.filter((id) => id !== sponsor.id))
+                      }
+                    }}
+                  >
+                    {sponsor.name}
+                  </CheckboxOption>
+                ))}
+              </div>
+            </div>
+          </Activity>
+        </div>
+      </div>
     </div>
   )
 }
